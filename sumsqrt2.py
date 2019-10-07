25d496cd74c96cefe7098826dec41120a7a942d2
gkaur37 <54698652+gkaur37@users.noreply.github.com>
2019-10-06 22:12:59 -0700

    Add files via upload

---
+++ sumsqrt2.py
@@ -0,0 +1,10 @@
+import math, time
+import numpy as np
+t=time.process_time()
+max=100000000
+sum=np.sum(np.sqrt(np.arange(1,max+1)))
+elapsed_time=time.process_time()-t
+print("Sum of sqrt of first ",max,"numbers=",sum,"time=",elapsed_time)
+
+#time= 2.662598
+#this is about sixteen times faster than using the explicit loop.
